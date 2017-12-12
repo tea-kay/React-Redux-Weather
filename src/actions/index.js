ccde014008c44bae9e4b5e2d6c43498fc197edb4
Timothy Kim <tkim1202@gmail.com>
2017-12-12 20:47:43 +0900

    Added axios for ajax and built url query string

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
 import { keys } from '../.keys.js';
+
+const API_KEY = WEATHER_KEY;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
